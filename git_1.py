e14d356c5393799a0ffa5b375ca9d179d893f5ff
dibirov <dibirov@ya.ru>
2022-11-19 15:19:20 +0300

    Снова первый print появился

--- git_1.py
+++ git_1.py
@@ -1,7 +1,38 @@
-def main():
-    print('My first git program')
+import sys
 
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+WINDOWS_SIZE = [1080, 768]
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)  # Загружаем дизайн
+        self.setGeometry(300, 150, *WINDOWS_SIZE)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.drawEllipse()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
