eb64d17b5d53a4d877ca4a4ab0090b395139315c
dibirov <dibirov@ya.ru>
2022-11-19 15:37:08 +0300

    это круги

--- git_1.py
+++ git_1.py
@@ -5,7 +5,13 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from random import randint
 
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 WINDOWS_SIZE = [1080, 768]
+
 
 class MyWidget(QMainWindow):
     def __init__(self):
@@ -28,11 +34,15 @@ class MyWidget(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.drawEllipse()
+        qp.setPen(QColor(255, 255, 0))
+        qp.drawEllipse(randint(0, WINDOWS_SIZE[0]),
+                       randint(0, WINDOWS_SIZE[1]),
+                       randint(0, 300), randint(0, 300))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = except_hook
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
